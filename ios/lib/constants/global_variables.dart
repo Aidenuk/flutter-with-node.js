ea07cbcf4e7563a12b6c3672ef0f1ad122741014
Aidenuk <darong0307@gmail.com>
2024-11-14 00:16:11 +0900

    setup flutter

---
+++ ios/lib/constants/global_variables.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GlobalVariables {
+  // COLORS
+  static const appBarGradient = LinearGradient(
+    colors: [
+      Color.fromARGB(255, 29, 201, 192),
+      Color.fromARGB(255, 125, 221, 216),
+    ],
+    stops: [0.5, 1.0],
+  );
+}
